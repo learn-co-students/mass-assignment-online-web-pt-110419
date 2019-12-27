e5e2ab03ce4613d115ec3e60199ae0a44bdf5622
Daniel Reina <dannyflatiron@gmail.com>
2019-12-27 01:39:02 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,16 @@
+require 'pry'
 class Person
-  #your code here
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_width, :pant_length
+
+
+  def initialize(attributes)
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+    # attributes is a hash
+    # iterate over each key, value pair in the attributes hash.
+    # the key is the setter method
+    # the value is the argument passed into the setter method
+    # .send calls the method with the passed argument
+    # binding.pry
+  end
+
 end
